b93456faea0cd3413844b556b886eb508877675e
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-07-29 13:30:23 +0500

    Created 3 Cards with the normal approach

--- 04-React/03-Props/src/App.jsx
+++ 04-React/03-Props/src/App.jsx
@@ -4,7 +4,22 @@ function App() {
 
   return (
   <>
-  <h1>Props</h1>
+  <div className="main-container">
+    <div className="cards">
+      <div className="card-1 card">
+      <h3>Card -1 </h3>
+      <p>This is the description of the Card 1</p>
+    </div>
+    <div className="card-2 card">
+       <h3>Card -2 </h3>
+      <p>This is the description of the Card 2</p>
+    </div>
+    <div className="card-3 card">
+       <h3>Card -3 </h3>
+      <p>This is the description of the Card 3</p>
+    </div>
+    </div>
+  </div>
   </>)
 }
 
