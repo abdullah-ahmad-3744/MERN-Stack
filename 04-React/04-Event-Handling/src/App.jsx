8417895eca26f50051892975be303c0ec6cafceb
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-07-30 14:03:24 +0500

    Practicing Event handling in React

---
+++ 04-React/04-Event-Handling/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="main-container">
+      <h1>Event Handling in React</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
