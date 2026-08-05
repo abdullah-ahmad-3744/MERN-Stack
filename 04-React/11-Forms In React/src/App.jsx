cde0bc7e1055735e6187a46a7519de2161b18557
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-08-06 02:27:24 +0500

    Handling multiple states of form in React

--- 04-React/11-Forms In React/src/App.jsx
+++ 04-React/11-Forms In React/src/App.jsx
@@ -1,10 +1,47 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  // const [firstName, setFirstName] = useState('')
+  // const [lastName,  setLastName] = useState('')
 
+  // function firstNameChangeHandler(event) {
+  //   console.log(event.target.value)
+  //   setFirstName(event.target.value)
+  // }
+
+  // function lastNameChangeHandler(event){
+  //   console.log(event.target.value)
+  //   setLastName(event.target.value)
+  // }
+  const [formData, setFormData] = useState(
+    {
+      firstName:'', lastName: '',email: ''
+    }
+  )
+
+  function changeHandler(event) {
+    setFormData(previousFormData => {
+      return {
+        ...previousFormData,
+        [event.target.name] : event.target.value
+      }
+    })
+  }
+  console.log(formData.firstName);
+  console.log(formData.lastName);
+  console.log(formData.email);
   return (
     <div>
-      <h1>Forms In React</h1>
+      <form action="">
+        <input  type="text"  name="firstName"  placeholder='first name'  onChange={changeHandler} />
+        <br />
+        <br />
+        <input  type="text"  name='lastName'   placeholder='last name'   onChange={changeHandler}/>
+        <br />
+        <br />
+        <input type="email" name="email" id="" placeholder='enter your email' onChange={changeHandler}/>
+      </form>
     </div>
   )
 }
