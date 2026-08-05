7b5762424387c704c099ec08e70cbf73e7d82ded
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-08-06 03:15:48 +0500

    checkboxes and radio buttons are handeledk

--- 04-React/11-Forms In React/src/App.jsx
+++ 04-React/11-Forms In React/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   // const [firstName, setFirstName] = useState('')
@@ -14,36 +14,105 @@ function App() {
   //   console.log(event.target.value)
   //   setLastName(event.target.value)
   // }
-  const [formData, setFormData] = useState(
-    {
-      firstName:'', lastName: '',email: ''
-    }
-  )
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comments: "",
+    isVisible: true,
+  });
 
   function changeHandler(event) {
-    setFormData(previousFormData => {
+    const { name, value, checked, type } = event.target;
+    setFormData((previousFormData) => {
       return {
         ...previousFormData,
-        [event.target.name] : event.target.value
-      }
-    })
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
   }
-  console.log(formData.firstName);
-  console.log(formData.lastName);
-  console.log(formData.email);
+  console.log(formData);
   return (
     <div>
       <form action="">
-        <input  type="text"  name="firstName"  placeholder='first name'  onChange={changeHandler} />
+        <input
+          type="text"
+          name="firstName"
+          placeholder="first name"
+          onChange={changeHandler}
+          value={formData.firstName}
+        />
         <br />
         <br />
-        <input  type="text"  name='lastName'   placeholder='last name'   onChange={changeHandler}/>
+        <input
+          type="text"
+          name="lastName"
+          placeholder="last name"
+          onChange={changeHandler}
+          value={formData.lastName}
+        />
         <br />
         <br />
-        <input type="email" name="email" id="" placeholder='enter your email' onChange={changeHandler}/>
+        <input
+          type="email"
+          name="email"
+          id=""
+          placeholder="enter your email"
+          onChange={changeHandler}
+          value={formData.email}
+        />
+
+        <br />
+        <br />
+
+        <textarea
+          name="comments"
+          placeholder="Enter your comments"
+          value={formData.comments}
+          onChange={changeHandler}
+        ></textarea>
+
+        <br />
+        <br />
+        <input
+          type="checkbox"
+          name="isVisible"
+          checked={formData.isVisible}
+          onChange={changeHandler}
+        />
+        <label htmlFor="isVisible" id="isVisible">
+          Am I visible
+        </label>
+        <br />
+        <br />
+
+        <fieldset>
+          <legend>Mode : </legend>
+          <label htmlFor="online-mode">Online Mode : </label>
+        <input
+          type="radio"
+          name="mode"
+          id="online-mode"
+          value="online-mode"
+          checked = {formData.mode === "online-mode"}
+          onChange={changeHandler}
+        />
+        <br />
+        <br />
+        <label htmlFor="offline-mode">Offline Mode : </label>
+        <input
+          type="radio"
+          name="mode"
+          id="offline-mode"
+          value="offline-mode"
+          checked={formData.mode === "offline-mode"}
+          onChange={changeHandler}
+        />
+        </fieldset>
+        
       </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
