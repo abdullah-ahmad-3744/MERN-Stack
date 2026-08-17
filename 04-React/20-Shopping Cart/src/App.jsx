b1a28f6b39005883d68869d00acb6aae18fc7f60
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-08-17 23:19:16 +0500

    Shopping Cart project started

---
+++ 04-React/20-Shopping Cart/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-black text-white text-center text-2xl font-bold py-2'>Shopping Cart Project</h1>
+    </div>
+  )
+}
+
+export default App
