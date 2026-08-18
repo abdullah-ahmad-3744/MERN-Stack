8c668afdc57a6a05e97df6929c8e33d3f3c2e733
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-08-18 23:10:43 +0500

    Count methods moved to Context

--- 04-React/17-Counter App (Context API)/src/context/CountContext.jsx
+++ 04-React/17-Counter App (Context API)/src/context/CountContext.jsx
@@ -6,11 +6,24 @@ const CountContext = createContext()
 
 function CountContextProvider({children}){
     const [count,setCount] = useState(1)
+
+     function incrementHandler(){
+        setCount(count + 1)
+    }
+    function decrementHandler(){
+        setCount(count - 1)
+    }
+    function resetHandler(){
+        setCount(0)
+    }
     return(
         <CountContext.Provider
         value={{
-            count: count,
-            setCount: setCount
+            count,
+            setCount,
+            resetHandler,
+            incrementHandler,
+            decrementHandler
         }}
         >
             {children}
