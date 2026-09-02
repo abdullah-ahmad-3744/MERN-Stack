f0e5d41f78b65832aa049cf497f350b46ad06dcc
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-09-03 02:29:48 +0500

    import and export

---
+++ 02-Backend/02-Import_and_Export/index.js
@@ -0,0 +1,9 @@
+import sum from "./sum.js";
+import sub from "./sub.js";
+import mul from "./mul.js";
+
+
+console.log('I am index file');
+console.log(`Sum : ${sum(10,10)}`);
+console.log(`Sub : ${sub(30,12)}`);
+console.log(`Mul : ${mul(20,5)}`);
