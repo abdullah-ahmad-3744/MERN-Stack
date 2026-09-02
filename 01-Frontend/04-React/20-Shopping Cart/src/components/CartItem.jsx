c2a70b2109f2fb760f4c209f85e57859cb73e906
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-09-02 12:01:29 +0500

    Project basic file structure

---
+++ 01-Frontend/04-React/20-Shopping Cart/src/components/CartItem.jsx
@@ -0,0 +1,8 @@
+function CartItem(){
+    return (
+        <div>
+            <h1>CartItem</h1>
+        </div>
+    )
+}
+export default CartItem
---
+++ 01-Frontend/04-React/20-Shopping Cart/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+function Home(){
+    const API_URL = 'https://fakestoreapi.com/products'
+    return(
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+export default Home
